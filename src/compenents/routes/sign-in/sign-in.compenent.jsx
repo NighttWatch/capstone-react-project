874a481d7e79c5ca1a9edfad7d0077f4355bbb91
NighttWatch <serdartekin0721@gmail.com>
2022-06-05 13:00:02 +0300

    firebase-auth-storage

---
+++ src/compenents/routes/sign-in/sign-in.compenent.jsx
@@ -0,0 +1,19 @@
+import { signInWithGoogleopup, createUserDocumentFromAuth  } from "../../../utils/firebase/firebase.utils";
+
+
+const SignIn = () => {
+    const logGoogleUser = async () => {
+        const { user } = await signInWithGoogleopup();
+        const userDocRef = await createUserDocumentFromAuth(user)
+    }
+    return (
+        <div>
+            <h1>Sign In Page </h1>
+            <button onClick={logGoogleUser}>
+                Sign in with Google Popup
+            </button>
+        </div>
+    )
+};
+
+export default SignIn;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,57 @@
+import { initializeApp } from 'firebase/app'; 
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC-hwLch13gS2kJudUqprFEIV62HWLtXbg",
+    authDomain: "crwn-clothing-db-a6403.firebaseapp.com",
+    projectId: "crwn-clothing-db-a6403",
+    storageBucket: "crwn-clothing-db-a6403.appspot.com",
+    messagingSenderId: "1010175421898",
+    appId: "1:1010175421898:web:2ef6b10ec9f2d4e8937333"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const providerGoogle = new GoogleAuthProvider();
+
+providerGoogle.setCustomParameters({
+    prompt: "select_account",
+});
+
+
+export const auth = getAuth();
+export const signInWithGoogleopup = () => signInWithPopup(auth, providerGoogle)
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef)
+
+    console.log(userSnapshot)
+
+    if(!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                email,
+                displayName,
+                createdAt
+            })
+        } catch (error) {
+            console.log('error creating the user', error.message);
+        }
+    }
+
+    return userDocRef;
+    //if user data exits
+    // if user data not exits
+    // create 
+    //reutrn userDocRef
+}
